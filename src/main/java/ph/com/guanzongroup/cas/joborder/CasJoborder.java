3545209a4ac61c7373e6c89c1814ade74df42f95
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2026-02-26 09:29:57 +0800

    Initial commit

---
+++ src/main/java/ph/com/guanzongroup/cas/joborder/CasJoborder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ph.com.guanzongroup.cas.joborder;
+
+/**
+ *
+ * @author Michael Cuison
+ */
+public class CasJoborder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
